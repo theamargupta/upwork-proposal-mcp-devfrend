dfa4f76976aae5316b88b2d6f04094c49a0c4de0
theamargupta <theamargupta.tech@gmail.com>
2026-04-13 16:31:41 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import "./globals.css"
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en"><body>{children}</body></html>
+  return <html lang="en" className={cn("font-sans", geist.variable)}><body>{children}</body></html>
 }
